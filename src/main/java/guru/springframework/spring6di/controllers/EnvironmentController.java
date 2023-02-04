bee15038bd42d6523dbd757612292065fc2d33d2
John Thompson <john@springframework.guru>
2023-02-04 10:21:31 -0500

    Updating Spring Boot Version and README

---
+++ src/main/java/guru/springframework/spring6di/controllers/EnvironmentController.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.EnvironmentService;
+import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EnvironmentController {
+
+    private final EnvironmentService environmentService;
+
+    public EnvironmentController(EnvironmentService environmentService) {
+        this.environmentService = environmentService;
+    }
+
+    public String getEnvironment() {
+        return "You are in " + environmentService.getEnv() + " Environment";
+    }
+}
